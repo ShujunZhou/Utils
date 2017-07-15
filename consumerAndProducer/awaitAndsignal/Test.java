8b14969a40cc9b9a164548a24de6b6fd3ed742b2
Shu <1129220400@qq.com>
2017-07-15 15:58:25 +0800

    lock,Condition实现

---
+++ consumerAndProducer/awaitAndsignal/Test.java
@@ -0,0 +1,31 @@
+package cn.smart.test.consumerAndProducer.awaitAndsignal;
+
+import cn.smart.test.consumerAndProducer.Consumer;
+import cn.smart.test.consumerAndProducer.Producer;
+import cn.smart.test.consumerAndProducer.StorageInter;
+import cn.smart.test.consumerAndProducer.awaitAndsignal.Storage;
+import java.util.concurrent.*;
+
+/**
+ * Created by shu on 2017/7/15.
+ * 使用数据库连接池进行测试
+ */
+public class Test {
+    public static void main(String[] args) {
+        StorageInter storage = new Storage();
+
+        //生产者线程池
+        ExecutorService producerPool = new ThreadPoolExecutor(4, 5, 2, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>(10), Executors.defaultThreadFactory(), new ThreadPoolExecutor.DiscardPolicy());
+        //消费者线程池
+        ExecutorService consumerPool = new ThreadPoolExecutor(4, 5, 2, TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(), Executors.defaultThreadFactory(), new ThreadPoolExecutor.DiscardPolicy());
+
+        for (int i = 0; i < 4; ++i) {
+            producerPool.submit(new Producer(storage, 10));
+        }
+        for (int i = 0; i < 5; ++i) {
+            consumerPool.submit(new Consumer(storage, 8));
+        }
+    }
+}
